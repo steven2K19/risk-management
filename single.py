b655c38c55984b316d4cf7a48db777d42118ddb8
Steven Wang <e_wanghe@hotmail.com>
2020-02-21 15:44:32 -0500

    Update single.py

--- single.py
+++ single.py
@@ -2,28 +2,34 @@ import yfinance as yf
 import numpy as np
 from datetime import date
 from scipy.stats import kurtosis, skew
-from scipy import stats
 
 symbol = "MSFT"    
 start = date(2010,1,1)
 end = date(2020,1,1)
 stock = yf.Ticker(symbol)
 ohlc = stock.history(start=start, end=end)
-ret = ohlc['Close'].pct_change().dropna()
 
-mu = np.mean(ret)
-sig = np.std(ret)
-skw = skew(ret)
-exkurt = kurtosis(ret)        
-kurt= exkurt + 3   
-sigm = sig*np.sqrt(21)
-siga = sig*np.sqrt(252)
-norm_pvalue = stats.shapiro(ret)[1]
-ret_d = ret.loc[ret<mu]
-sigd = np.std(ret_d)
-sigdm = sigd*np.sqrt(21)
-sigda = sigd*np.sqrt(252)
+# return daily, monthly, annually
+ret_day = ohlc['Close'].pct_change().dropna()
+ret_month = ohlc['Close'].asfreq('M',method='ffill').pct_change().dropna()
+ret_year = ohlc['Close'].asfreq('A',method='ffill').pct_change().dropna()
+
+# standard deviation daily, monthly, annualy; skew, kurtosis 
+std_day = np.std(ret_day)
+std_month= np.std(ret_month)
+std_year = std_day*np.sqrt(12)
+skw = skew(ret_month)
+exkurt = kurtosis(ret_month)
+
+# max drawdown 
 rollmax = ohlc.Close.rolling(window=252, min_periods=1).max()
 rollmin = ohlc.Close.rolling(window=252, min_periods=1).min()
 mdd = (rollmin - rollmax)/rollmax
 mdd = mdd.drop_duplicates(keep='last').min()
+
+# downside standard deviation  
+mu = np.mean(ret_day)
+ret_dn = ret_day.loc[ret_day<mu]
+std_dn = np.std(ret_dn)
+std_dnm = std_dn*np.sqrt(21)
+std_dny = std_dn*np.sqrt(252)
