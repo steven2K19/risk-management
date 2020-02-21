9363e34cccfde9621a5b7f3f6964b21d305f3734
Steven Wang <e_wanghe@hotmail.com>
2020-02-21 14:55:30 -0500

    Update single.py

--- single.py
+++ single.py
@@ -1,27 +1,15 @@
-import pandas_datareader as pdr
+import yfinance as yf
 import numpy as np
-from datetime import date, timedelta 
+from datetime import date
 from scipy.stats import kurtosis, skew
 from scipy import stats
 
-def get_beta(tickers):
-    df = pdr.DataReader(tickers, 'yahoo',start,end)
-    df = df ['Adj Close'].resample('BMS').first()
-    df['ret'] = df['MSFT'].pct_change()
-    df['retx'] = df['SPY'].pct_change()
-    df = df.dropna()
-    cov = df[['ret','retx']].cov()
-    cov = cov.iloc[0,1]
-    spy_var = df['retx'].var()
-    beta = cov/spy_var
-    return beta
-
-symbol = "MSFT"
-today = date.today()      
-start = today-timedelta(days=365)  
-end = today
-ohlc = pdr.DataReader(symbol, 'yahoo',start,end) 
-ret = ohlc['Adj Close'].pct_change().dropna()
+symbol = "MSFT"    
+start = date(2010,1,1)
+end = date(2020,1,1)
+stock = yf.Ticker(symbol)
+ohlc = stock.history(start=start, end=end)
+ret = ohlc['Close'].pct_change().dropna()
 
 mu = np.mean(ret)
 sig = np.std(ret)
@@ -39,4 +27,3 @@ rollmax = ohlc.Close.rolling(window=252, min_periods=1).max()
 rollmin = ohlc.Close.rolling(window=252, min_periods=1).min()
 mdd = (rollmin - rollmax)/rollmax
 mdd = mdd.drop_duplicates(keep='last').min()
-beta=get_beta(sym)
