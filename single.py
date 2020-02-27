6241041067f813656e5ea571396ae4be0985c0bb
Steven Wang <e_wanghe@hotmail.com>
2020-02-27 15:37:34 -0500

    Update single.py

--- single.py
+++ single.py
@@ -139,3 +139,5 @@ inf = [hpr, mu_day, mu_month, mu_year, std_day, std_month, std_year, skw, exkurt
 inf= inf + metric
 inf = [round(item,4) for item in inf]
 inf.append(symbol)
+
+names = [hpr, mu_day, mu_month, mu_year, std_day, std_month, std_year, skw, exkurt, mdd, std_dn, std_dnm, std_dny, beta, var_para, var_hist, var_mod, var_con, var_mc, sharpe, treynor, rovar, sortino, jenalpha, msquare, ff5rsquare, alpha, mkrf, smb, hml, rmw, cma]
