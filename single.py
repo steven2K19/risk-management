96b16eec9f77e139b8399be7df6ae7a768f3aff9
Steven Wang <e_wanghe@hotmail.com>
2020-02-27 14:24:50 -0500

    Update single.py

--- single.py
+++ single.py
@@ -2,7 +2,8 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from datetime import date, timedelta
-from scipy.stats import kurtosis, skew
+from scipy.stats import kurtosis, skew, norm
+import pandas_datareader as pdr
 
 symbol = "MSFT"    
 start = date(2010,1,1)
@@ -20,6 +21,9 @@ divid = ohlc[ohlc.Dividends>0]
 divid['repurchase_price']= (divid.Close + divid.Open + divid.Low + divid.Close)/4
 repurchase_share = (divid.Dividends/divid.repurchase_price).sum()
 hpr = (1+repurchase_share)*ohlc.Close.iloc[-1]/ohlc.Close.iloc[0]-1
+mu_day = np.mean(ret_day)
+mu_month = np.mean(ret_month)
+mu_year = np.mean(ret_year)
 
 # standard deviation daily, monthly, annualy; skew, kurtosis 
 std_day = np.std(ret_day)
@@ -35,44 +39,81 @@ mdd = (rollmin - rollmax)/rollmax
 mdd = mdd.drop_duplicates(keep='last').min()
 
 # downside standard deviation  std_dn
-mu = np.mean(ret_day)
-ret_dn = ret_day.loc[ret_day<mu]
+ret_dn = ret_day.loc[ret_day<mu_day]
 std_dn = np.std(ret_dn)
 std_dnm = std_dn*np.sqrt(21)
 std_dny = std_dn*np.sqrt(252)
 
-# 5 year monthly beta
-def get_beta(end, tick):
-    start = end -timedelta(days=365*5) - pd.tseries.offsets.BusinessDay(n=2)
+# monthly beta
+def get_beta_month(sym,end):
+    start = end -timedelta(days=365*10) - pd.tseries.offsets.BusinessDay(n=2)
     spy= yf.Ticker('SPY')
     spy= spy.history(start=start, end=end)
-    tick= yf.Ticker(tick)
+    tick= yf.Ticker(symbol)
     tick = tick.history(start=start, end=end)
-    df = df ['Close'].resample('BMS').first()
-    df['ret'] = df['MSFT'].pct_change()
-    df['retx'] = df['SPY'].pct_change()
-    df = df.dropna()
-    cov = df[['ret','retx']].cov()
+    spy = spy['Close'].resample('BM').first()
+    tick = tick['Close'].resample('BM').first()
+    spy = spy.pct_change().dropna()
+    spy_var = spy.var()
+    tick = tick.pct_change().dropna()
+    df = pd.concat([spy,tick], axis=1)
+    df.columns = ['spy','tick']
+    cov = df[['spy','tick']].cov()
     cov = cov.iloc[0,1]
-    spy_var = df['retx'].var()
     beta = cov/spy_var
     return beta
+beta = get_beta_month(symbol, end)
 
-############
-symbol = "MSFT"    
-end = date(2020,1,1)
+def get_var(ret):
+    cnf = 0.95
+    cna= 1-cnf
+    # Parametric VaR      VaR= share*price*VaR*np.sqrt(days)
+    mu = np.mean(ret)
+    sig = np.std(ret)
+    skw = skew(ret)
+    exkurt = kurtosis(ret)        
+    kurt= exkurt + 3  
+    var_para = abs(norm.ppf(cna, mu, sig)) 
 
-start = end -timedelta(days=365*5) - pd.tseries.offsets.BusinessDay(n=2)
-spy= yf.Ticker('SPY')
-spy= spy.history(start=start, end=end)
-tick= yf.Ticker(symbol)
-tick = tick.history(start=start, end=end)
-spy = spy['Close'].resample('BM').first()
-tick = tick['Close'].resample('BM').first()
-spy = spy.pct_change().dropna()
-tick = tick.pct_change().dropna()
+    # History VaR
+    cnf = 0.95
+    cna = 1-cnf
+    ret2 = np.sort(ret)
+    n = np.size(ret2)
+    lefttail = int(n*cna)
+    var_hist = abs(ret2[lefttail])
 
-cov = df[['ret','retx']].cov()
-# cov = cov.iloc[0,1]
-# spy_var = df['retx'].var()
-# beta = cov/spy_var
+    # Modified VaR 
+    z = norm.ppf(cna)
+    mod = z+1/6.*(z**2-1)*skw+1/24.*(z**3-3*z)*kurt-1/36.*(2*z**3-5*z)*skw**2
+    var_mod= abs(mu + mod*sig)
+
+    # CVaR  average 5% loss 
+    var_con = abs(ret2[ret2<= ret2[int(n*cna)]].mean())
+
+    # Monte Carlos VaR
+    simu=1000
+    ret3=np.random.normal(mu,sig,simu)
+    ret4=np.sort(ret3)
+    var_mc=abs(ret4[int(simu*cna)])
+    
+    x = [var_para, var_hist, var_mod, var_con, var_mc]
+    return x
+
+value_at_risk = get_var(ret_day)
+
+rf = pdr.DataReader('TB3MS','fred',start,end)
+rf = (rf['TB3MS'][-1]*0.01+1)**(1/365)-1
+spy = pdr.DataReader('SPY', 'yahoo',start,end)
+retspy = spy['Adj Close'].pct_change().dropna()
+muspy = np.mean(retspy)
+stdspy = np.std(retspy)
+
+sharpe = (mu_day-rf)/std_day
+treynor = (mu_day-rf)/beta
+rovar = mu_day/value_at_risk[0]
+sortino = (mu_day-rf)/std_dn  
+jenalpha = mu_day - (rf + beta*(muspy-rf))  
+msquare = (mu_day-rf)* stdspy/std_day - (muspy-rf)
+
+inf = [end, hpr, mu_day, mu_month, mu_year, ]
