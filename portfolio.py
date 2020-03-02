4503b2059ffa98af7b8b282ef5e453cd397615ec
Steven Wang <e_wanghe@hotmail.com>
2020-03-02 12:17:11 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,10 @@
-
-position = ['MSFT', 'AAPL', 'AMZN', 'FB', 'BRK.B', 'GOOGL', 'GOOG', 'JPM', 'JNJ','V',
-            'PG', 'MA', 'T', 'UNH', 'INTC', 'HD', 'BAC', 'VZ', 'XOM', 'DIS','KO', 'SPY']
+descrip = ['American Express Co', 'Apple Inc','Boeing Co', 'Caterpillar Inc', 'Cisco Systems Inc', 'Chevron Corp', 
+           'Exxon Mobil Corp, 'Goldman Sachs Group Inc','Home Depot Inc', 'International Business Machines Corp',
+           'Intel Corp', 'Johnson & Johnson', 'Coca-Cola Co', 'JPMorgan Chase & Co', 'McDonald’s Corp', '3M Co', 
+           'Merck & Co Inc', 'Microsoft Corp', 'Nike Inc', 'Pfizer Inc', 'Procter & Gamble Co','Travelers Companies Inc', 
+           'UnitedHealth Group Inc', 'United Technologies Corp', 'Verizon Communications Inc', 'Visa Inc', 'Walgreens', 
+           'Boots Alliance Inc', 'Walmart Inc', 'Walt Disney Co', 'Dow Inc']
+position = [AXP	AAPL	BA	CAT	CSCO	CVX	XOM	GS	HD	IBM	INTC	JNJ	KO	JPM	MCD	MMM	MRK	MSFT	NKE	PFE	PG	TRV	UNH	UTX	VZ	V	WBA	WMT	DIS	DOW]
 Weight = ['5.048089, 4.663278, 3.205393, 1.892134, 1.648823, 1.639413, 1.635108, 1.487732, 1.445913, 1.271095, 
           1.154213, 1.052356, 1.050994, 0.986659, 0.986573, 0.970731, 0.942499, 0.914919, 0.88898, 0.866105, 66.24899,100]
 
